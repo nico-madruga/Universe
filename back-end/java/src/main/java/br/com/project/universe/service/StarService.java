18a1c6a6c397c35d19fcfc34488c0f35ab745721
nico-madruga <nizinhoms@gmail.com>
2026-09-20 10:31:59 -0300

    feat: created class service with dependency injection

--- back-end/java/src/main/java/br/com/project/universe/service/StarService.java
+++ back-end/java/src/main/java/br/com/project/universe/service/StarService.java
@@ -2,8 +2,21 @@ package br.com.project.universe.service;
 
 import org.springframework.stereotype.Service;
 
+import br.com.project.universe.dto.StarRequestDTO;
+import br.com.project.universe.dto.StarResponseDTO;
+import br.com.project.universe.mapper.StarMapper;
+import br.com.project.universe.repository.StarRepository;
+
 @Service 
 public class StarService 
 {
+    StarRepository starRepo;
+    StarMapper starMap;
+
+    public StarService(StarRepository starRepo, StarMapper starMap)
+    {
+        this.starRepo = starRepo;
+        this.starMap = starMap;
+    }
 
 }
