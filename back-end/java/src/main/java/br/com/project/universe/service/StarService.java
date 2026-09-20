a42a81159ee4aebcb03ebf10c27cd7e39993725e
nico-madruga <nizinhoms@gmail.com>
2026-09-20 11:08:41 -0300

    feat: Implemented creating stars

--- back-end/java/src/main/java/br/com/project/universe/service/StarService.java
+++ back-end/java/src/main/java/br/com/project/universe/service/StarService.java
@@ -1,22 +1,32 @@
 package br.com.project.universe.service;
 
 import org.springframework.stereotype.Service;
+import java.util.List;
 
+import br.com.project.universe.entity.Star;
 import br.com.project.universe.dto.StarRequestDTO;
 import br.com.project.universe.dto.StarResponseDTO;
 import br.com.project.universe.mapper.StarMapper;
 import br.com.project.universe.repository.StarRepository;
 
+
 @Service 
 public class StarService 
 {
-    StarRepository starRepo;
-    StarMapper starMap;
+    private StarRepository starRepo;
+    private StarMapper starMap;
 
     public StarService(StarRepository starRepo, StarMapper starMap)
     {
         this.starRepo = starRepo;
         this.starMap = starMap;
+    }
+
+    public void createStar(StarRequestDTO newStar)
+    {
+        Star star = starMap.toEntity(newStar);
+
+        starRepo.save(star);
     }
 
 }
