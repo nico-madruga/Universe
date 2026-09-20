497a1cd0d81966ce9901f22a97fe6d2e0387af5c
nico-madruga <nizinhoms@gmail.com>
2026-09-20 10:52:00 -0300

    feat: Implemented to entity and to DTO conversion

--- back-end/java/src/main/java/br/com/project/universe/mapper/StarMapper.java
+++ back-end/java/src/main/java/br/com/project/universe/mapper/StarMapper.java
@@ -1,6 +1,30 @@
 package br.com.project.universe.mapper;
 
+import org.springframework.http.ResponseEntity;
+
+import br.com.project.universe.dto.StarRequestDTO;
+import br.com.project.universe.dto.StarResponseDTO;
+import br.com.project.universe.entity.Star;
+
 public class StarMapper 
 {
-    
+    public Star toEntity(StarRequestDTO starDTO)
+    {
+        Star starEntity = new Star(
+            starDTO.name(), 
+            starDTO.mass(), 
+            starDTO.temperature());
+
+        return starEntity;
+    }
+
+    public StarResponseDTO toDTO(Star star)
+    {
+        StarResponseDTO starResponse = new StarResponseDTO(
+            star.getName(),
+            star.getMass(),
+            star.getTemperature());
+
+            return starResponse;
+    }
 }
